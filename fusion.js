2bb9e2af84ae9446f29b4f4fd83ac89ace4bcb86
CKylinMC <Cansll@users.noreply.github.com>
2018-03-25 16:53:47 +0800

    Add files via upload

---
+++ fusion.js
@@ -0,0 +1,27 @@
+var app = {
+	version: 6,
+	ver: "v1.2",
+	apk: "https://github.com/Cansll/How-To-Search/releases/download/v1.2/app.apk",
+	msg: "更新：\n\n* 新增检查更新功能",
+	tempKey: 1,
+	test: function(clientVer){
+		if(clientVer<app.version){
+			if(confirm("APP有更新，是否前往更新？")){
+				location.replace("download.html");
+			}
+		}
+	},
+	testOnce: function(clientVer){
+		if(app.tempKey) return;
+		app.tempKey = 0;
+		if(clientVer<app.version){
+			if(confirm("APP有更新，是否前往更新？")){
+				location.replace("download.html");
+			}
+		}
+	},
+};
+var msgs = {
+	msg: false,
+	target: false
+};
