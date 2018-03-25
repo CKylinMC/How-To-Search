03603999fd64535a19a82db5419f661c20b40fc4
CKylinMC <Cansll@users.noreply.github.com>
2018-03-25 17:18:55 +0800

    fix bugs

--- fusion.js
+++ fusion.js
@@ -1,8 +1,8 @@
 var app = {
 	version: 6,
-	ver: "v1.2",
+	ver: "v1.2.6",
 	apk: "https://github.com/Cansll/How-To-Search/releases/download/v1.2/app.apk",
-	msg: "更新：\n\n* 新增检查更新功能",
+	msg: "更新：\n\n* 新增检查更新功能\n* 修复bug",
 	tempKey: 0,
 	test: function(clientVer){
 		if(clientVer<app.version){
@@ -14,11 +14,7 @@ var app = {
 	testOnce: function(clientVer){
 		if(app.tempKey) return;
 		app.tempKey = 1;
-		if(clientVer<app.version){
-			if(confirm("APP有更新，是否前往更新？")){
-				location.replace("download.html");
-			}
-		}
+		app.test(clientVer);
 	},
 };
 var msgs = {
